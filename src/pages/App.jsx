bac3aedd0128084614ac9c572732e448138b4177
muriloH421 <basiliomurilo421@gmail.com>
2023-08-18 09:29:19 -0300

    desenvolvido componente movie card adapt. no app.jsx criador arquivo movieCard movieCard.module.css falta codigo

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,27 +4,29 @@ import searchIcon from "../assets/search.svg";
 import { useState } from "react";
 
 import "./App.css";
+import MovieCard from "../components/movieCard/movieCard";
 import { useCallback } from "react";
 
 const App = () => {
-  const [searchTerm,setSearchTerm] = useState ("")
-
+  const [searchTerm, setSearchTerm] = useState("");
+const [movies,setMovies]= useState([])
   const apikey = "e4d577fa";
   const apiUrl = `https://omdbapi.com/?apikey=${apikey}`;
 
   useEffect(() => {
-    searchMovies("Batman")
+    searchMovies("Batman");
   }, []);
 
   const searchMovies = async (title) => {
-    const response = await fetch (`${apiUrl}&s=${title}`)
+    const response = await fetch(`${apiUrl}&s=${title}`);
     const data = await response.json();
- console.log(data);
+    console.log(data);
+    setMovies(data.Search);
   };
 
   const handleKeyPress = (e) => {
-    e.key === "Enter" && searchMovies(searchTerm)
-  }
+    e.key === "Enter" && searchMovies(searchTerm);
+  };
 
   // fetch(apiUrl)
   //   .then((Response) => Response.json())
@@ -37,12 +39,28 @@ const App = () => {
       </div>
       <div className="search">
         <input
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        onKeyDown={handleKeyPress}
-        placeholder="pesquise aqui " />
-        <img src={searchIcon} alt="Icone de pesquisa" onClick={()=>searchMovies(searchTerm)} />
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleKeyPress}
+          placeholder="pesquise aqui "
+        />
+        <img
+          src={searchIcon}
+          alt="Icone de pesquisa"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard  movies={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Nenhum filme encontrado 😢</h2>
+        </div>
+      )}
     </div>
   );
 };
