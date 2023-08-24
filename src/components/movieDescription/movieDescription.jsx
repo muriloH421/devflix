97ee85765ad1822bd1b5a8b6ba56ae37a9644539
muriloH421 <basiliomurilo421@gmail.com>
2023-08-24 14:10:02 -0300

    movieDescrition   finalizado

--- src/components/movieDescription/movieDescription.jsx
+++ src/components/movieDescription/movieDescription.jsx
@@ -12,7 +12,7 @@ const MovieDesc = ({ movies, click }) => {
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => setMovieDesc(data))
-      .catch((error) => console.log("Erro: ", error));
+      .catch((error) => console.error("Erro: ", error));
   }, [apiUrl]);
   const movie = MovieDesc;
 
@@ -21,7 +21,7 @@ const MovieDesc = ({ movies, click }) => {
     <div className={styles.modalBackdrop} onClick={click}>
       <div className={styles.movieModal} onClick={(e) => e.stopPropagation()}>
         <div className={styles.movieInfo}>
-          <img src={movie.Poster} alt={movie.Title} />
+          <img src={movies.Poster} alt={movie.Title} />
           <button className={styles.btnClose} onClick={click}>
             X
           </button>
@@ -58,5 +58,6 @@ const MovieDesc = ({ movies, click }) => {
       </div>
     </div>
   );
+  
 };
 export default MovieDesc;
